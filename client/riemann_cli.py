38da59adfa3ce87fd86893ffd3d898ff8ccd5e1a
Benoit Capitanio <benoit.capitanio@gmail.com>
2018-01-24 23:12:57 +0100

    client: still have an issue with the riemann client part: TypeError: must be str, not bytes for the value part of the event

---
+++ client/riemann_cli.py
@@ -0,0 +1,57 @@
+import random
+
+import bernhard
+
+
+def get_service_name():
+    service_list = [
+        'cpu',
+        'memory',
+        'disk usage',
+        'swap']
+
+    return random.choice(service_list)
+
+
+def get_event_value():
+    value_list = [
+        10,
+        50,
+        75,
+        80,
+        85,
+        90,
+        95]
+
+    return random.choice(value_list)
+
+
+def get_hostname():
+    hostname_list = [
+        'web001',
+        'web002',
+        'db001',
+        'db002']
+
+    return random.choice(hostname_list)
+
+
+def main():
+    client = bernhard.Client(host='127.0.0.1',
+                             port=5555)
+    event_service = get_service_name()
+    event_value = get_event_value()
+    event_hostname = get_hostname()
+
+    print('host: {}, service: {}, value: {}'.format(
+        event_hostname,
+        event_service,
+        str(event_value)))
+
+#    client.send({'host': event_hostname,
+#                 'service': event_service,
+#                 'value': str(event_value)})
+
+
+if __name__ == '__main__':
+    main()
